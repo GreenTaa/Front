1501d7cbf1c8d7a6d21f7b9a87433e804ecfae94
Aziz Sahnoun <mohamedaziz.sahnoun@esprit.tn>
2021-08-19 09:29:47 +0100

    last cleaning

--- src/App.js
+++ src/App.js
@@ -16,6 +16,41 @@ import main from "./views";
 import app from "./views/app";
 import user from "./views/user";
 import error from "./views/error";
+import { Home } from "./Pages/Home";
+import DigitalMarketing from "./Pages/Digital-marketing";
+import PaymentProcessing from "./Pages/Payment-processing";
+import HRManagement from "./Pages/HR-Management";
+import Startup from "./Pages/Startup";
+import About from "./Pages/About";
+import Service from "./Pages/Service";
+import Process from "./Pages/Process";
+import Team from "./Pages/Team";
+import Portfolio2col from "./Pages/Portfolio-2col";
+import Portfolio3col from "./Pages/Portfolio-3col";
+import Portfoliofull4col from "./Pages/Portfolio-fullwidth-4col";
+import PortfolioSingle from "./Pages/PortfolioSingle";
+import Bloglist from "./Pages/Bloglist";
+import BlogSingle from "./Pages/BlogSingle";
+import Contact from "./Pages/Contact";
+import ScrollToTopRoute from "./ScrollToTopRoute";
+import Landing from "./Pages/Landing";
+import homesupport from "./Pages/home-support";
+import homeERP from "./Pages/Home-ERP";
+import homeHosting from "./Pages/HomeHosting";
+import homeSecurity from "./Pages/HomeSecurity";
+import homeSoftwareDark from "./Pages/Home-software-dark";
+import HomeAppShowcase from "./Pages/Home-app-showcase";
+import HomeCloud from "./Pages/Home-cloud";
+import HomeTracking from "./Pages/Home-tracking";
+import HomeEvent from "./Pages/Home-event";
+import HomeChat from "./Pages/Home-chat";
+import Price from "./Pages/Price";
+import Faq from "./Pages/Faq";
+import ServiceDetails from "./Pages/ServiceDetails";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import BlogGridPage from "./Pages/BlogGridPage";
+
 
 const AuthRoute = ({ component: Component, authUser, ...rest }) => (
   <Route
@@ -52,10 +87,63 @@ class App extends Component {
             <Router>
               <Switch>
                 <AuthRoute path="/app" authUser={loginUser} component={app} />
-                <Route path="/user" component={user} />
                 <Route path="/error" exact component={error} />
                 <Route path="/" exact component={main} />
-                <Redirect to="/error" />
+
+          <ScrollToTopRoute
+            path="/Digital-marketing"
+            component={DigitalMarketing}
+          />
+          <ScrollToTopRoute
+            path="/Payment-processing"
+            component={PaymentProcessing}
+          />
+                    <ScrollToTopRoute path="/home" component={Home} />
+
+          <ScrollToTopRoute path="/HR-Management" component={HRManagement} />
+          <ScrollToTopRoute path="/Startup" component={Startup} />
+          <ScrollToTopRoute path="/About" component={About} />
+          <ScrollToTopRoute path="/Service" component={Service} />
+          <ScrollToTopRoute path="/Process" component={Process} />
+          <ScrollToTopRoute path="/Team" component={Team} />
+          <ScrollToTopRoute path="/Portfolio-2col" component={Portfolio2col} />
+          <ScrollToTopRoute path="/Portfolio-3col" component={Portfolio3col} />
+          <ScrollToTopRoute
+            path="/Portfolio-fullwidth-4col"
+            component={Portfoliofull4col}
+          />
+          <ScrollToTopRoute
+            path="/PortfolioSingle"
+            component={PortfolioSingle}
+          />
+          <ScrollToTopRoute path="/Bloglist" component={Bloglist} />
+          <ScrollToTopRoute path="/BlogSingle" component={BlogSingle} />
+          <ScrollToTopRoute path="/BlogGridPage" component={BlogGridPage} />
+          <ScrollToTopRoute path="/Contact" component={Contact} />
+          <ScrollToTopRoute path="/Landing" component={Landing} />
+          <ScrollToTopRoute path="/home-support" component={homesupport} />
+          <ScrollToTopRoute path="/HomeHosting" component={homeHosting} />
+          <ScrollToTopRoute path="/Home-ERP" component={homeERP} />
+          <ScrollToTopRoute path="/HomeSecurity" component={homeSecurity} />
+          <ScrollToTopRoute
+            path="/HomeAppShowcase"
+            component={HomeAppShowcase}
+          />
+          <ScrollToTopRoute
+            path="/Home-software-dark"
+            component={homeSoftwareDark}
+          />
+          <ScrollToTopRoute path="/Home-cloud" component={HomeCloud} />
+          <ScrollToTopRoute path="/Home-Tracking" component={HomeTracking} />
+          <ScrollToTopRoute path="/Home-event" component={HomeEvent} />
+          <ScrollToTopRoute path="/Home-chat" component={HomeChat} />
+          <ScrollToTopRoute path="/Price" component={Price} />
+          <ScrollToTopRoute path="/Faq" component={Faq} />
+          <ScrollToTopRoute path="/ServiceDetails" component={ServiceDetails} />
+          <ScrollToTopRoute path="/SignIn" component={SignIn} />
+          <ScrollToTopRoute path="/SignUp" component={SignUp} />
+          <Redirect to="/error" />
+
               </Switch>
             </Router>
           </React.Fragment>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ import "react-circular-progressbar/dist/styles.css";
 import "react-perfect-scrollbar/dist/css/styles.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import "react-table/react-table.css";
-
+import "./App.css";
+import "./assets/themify-icon/themify-icons.css";
+import "./assets/simple-line-icon/simple-line-icons.css";
+import "./assets/font-awesome/css/all.css";
+import "./assets/elagent/style.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./assets/animate.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/main.css";
+import "./assets/responsive.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "popper.js";
 import { isMultiColorActive, defaultColor } from "./constants/defaultValues";
 const color =
   isMultiColorActive && localStorage.getItem("themeColor")
